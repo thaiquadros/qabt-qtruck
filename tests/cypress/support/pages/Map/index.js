3150e7292a73add6b85b398566bbeafb6c340b62
Thaiana Quadros <thai_quadros@hotmail.com>
2022-08-29 16:01:44 -0400

    updating custom commands and create page for recommendation

--- tests/cypress/support/pages/Map/index.js
+++ tests/cypress/support/pages/Map/index.js
@@ -1,9 +1,19 @@
+
+
 class MapPage {
-    
+
     loggedUser(name) {
         cy.get('.logged-user')
             .should('be.visible')
             .should('have.text', `Olá, ${name}`)
     }
+
+    createLink() {
+        cy.get('a[href="/foodtrucks/create"]')
+            .should('be.visible')
+            .click()
+    }
+
 }
+
 export default new MapPage()
